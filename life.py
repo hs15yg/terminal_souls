3c35cb3d648cd3823b50587b70c4d98953523e53
hs15yg <hilayepes@gmail.com>
2026-03-20 07:54:13 -0500

    life finish

--- life.py
+++ life.py
@@ -2,28 +2,41 @@ BLACK = '\033[30m'
 RED = '\033[31m'
 RESET = '\033[0m'
 
-# utilizo random para que me escoja un numero aleatorio dentro del rango
+# i use random so it picks a random number within the range
 import random
-#creo una funcion para saber el porcentaje del daño del heroe y el enemigo
-def damage (min , max):
-    #le he puesto de nombre a la funcion damage
+# i create a function to calculate the damage percentage of the hero and the enemy
+def damage(life, min=10, max=25, malice = False):
+
+    # i named the function damage
     critical = random.randint(1 , 100)
-    #utilizo la condicionales para que tome una decision dependiendo si es falsa o verdadera
-    if critical >= 1 and critical <= 10:
-        combat = random.randint(min , max)*2
+    # i use conditionals so it makes a decision depending on whether it is true or false
+    if critical >= 1 and critical <= 10 and malice == True:
+        combat = random.randint(30 , 50)*2
     else:
         combat = random.randint(min , max)
-    #utilizo return para devolver el daño realizado
-    return combat
+    # i use return to return the damage dealt
+    
+    life -= combat
+    
+    return print("the hero has lost this part of life 😢" , combat )
 
-#funcion de barra de vida
-def life_bar (hp, name):
-    bar = ""   
-    for i in range(1 , hp , 10):
-        bar += "#"
-    if name.lower() == "hero":
-        return print(f"{name}: \nVida: {RED}{bar}{RESET}")
-    else:
-        return print(f"{name}: \nVida: {BLACK}{bar}{RESET}")
 
-life_bar(120, "Hero")
+# life bar function
+def life_bar (hp_hero , hp_enemy , enemy, hero):
+    hero_bar = ""   
+    for i in range(1, hp_hero , 10):
+        hero_bar += "#"
+
+    enemy_bar = "" 
+    for i in range (1, hp_enemy , 10):
+        enemy_bar += "#"
+
+    return print(f"💪{hero}: {RED}{hero_bar}{RESET} {hp_hero}% \n🦹{enemy}: {BLACK}{enemy_bar}{RESET} {hp_enemy}%")
+    
+
+
+
+life_bar (100, 120, "enemy","hero")
+
+damage(life=100)
+
